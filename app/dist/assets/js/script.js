fe19dcadb6446fa25f19d18e516e39eb3dd200ba
van <van1sergius@gmail.com>
2019-12-31 02:11:17 +0600

    header finally done

--- app/dist/assets/js/script.js
+++ app/dist/assets/js/script.js
@@ -2,7 +2,6 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.hamburger').classList.toggle('is-active');
     document.querySelector('.nav__item--active').classList.remove('.nav__item--active');
-    // document.querySelector('.nav__item--active').classList.toggle('.nav__item--active');
     var divs = document.querySelectorAll('.nav__item');
         for (var i = 0; i < divs.length; i++) {
             divs[i].classList.toggle('nav__item--visible');
