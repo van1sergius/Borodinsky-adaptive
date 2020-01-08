f12e72f346506bf33d9e8d98daef52dbbf2d7a7f
van <van1sergius@gmail.com>
2020-01-08 18:52:33 +0600

    features mobile done

---
+++ app/src/assets/js/fragments/features/slider.js
@@ -0,0 +1,11 @@
+//= ../../../../../../node_modules/slick-carousel/slick/slick.js
+
+$(document).ready(function(){
+    $('.features').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true
+    });
+  });
--- app/src/assets/js/script.js
+++ app/src/assets/js/script.js
@@ -1,1 +1,4 @@
+//= ../../../../node_modules/jquery/dist/jquery.js
+//= fragments/features/slider.js
 //= fragments/header/hamburger.js
+
