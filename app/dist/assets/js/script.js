39fd9a829d92372a1b7eb7116e0ee08ff430fe80
van <van1sergius@gmail.com>
2020-01-10 11:44:10 +0600

    initial commit

--- app/dist/assets/js/script.js
+++ app/dist/assets/js/script.js
@@ -13609,7 +13609,17 @@ return jQuery;
 }));
 
 $(document).ready(function(){
-    $('.features').slick({
+    $('.features__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true
+    });
+  });
+
+  $(document).ready(function(){
+    $('.reviews__slider').slick({
         arrows: false,
         dots: true,
         slidesToShow: 1,
--- app/src/assets/js/fragments/features/slider.js
+++ app/src/assets/js/fragments/features/slider.js
@@ -1,7 +1,17 @@
 //= ../../../../../../node_modules/slick-carousel/slick/slick.js
 
 $(document).ready(function(){
-    $('.features').slick({
+    $('.features__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        mobileFirst: true
+    });
+  });
+
+  $(document).ready(function(){
+    $('.reviews__slider').slick({
         arrows: false,
         dots: true,
         slidesToShow: 1,
