3c0fc234c55020f1661991e795ea6c0d2fe7f505
van <van1sergius@gmail.com>
2020-01-17 11:10:34 +0600

    actve class togle fixed

--- app/dist/assets/js/script.js
+++ app/dist/assets/js/script.js
@@ -13978,3 +13978,19 @@ document.querySelector('.hamburger').addEventListener('click', function() {
             divs[i].classList.toggle('nav__link--visible');
     }
 })
+
+$(document).ready(function() {
+	// get current URL path and assign 'active' class
+    // var pathname = window.location.pathname;
+    // $('.nav__list > .nav__item > a').removeClass('nav__link--active');
+    // $('.nav__list > .nav__item > a[href="'+pathname+'"]').addClass('nav__link--active');
+    // console.log(pathname);
+
+})
+
+var loc = window.location.pathname;
+if (loc !== '/') {
+    $('.nav__item').find('a').each(function() {
+        $(this).toggleClass('nav__link--active', $(this).attr('href') == loc);
+     });
+}
