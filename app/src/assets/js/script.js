33218caab1ede68005c89bd2f15eaf58fb0bbd50
van <van1sergius@gmail.com>
2020-01-17 12:40:01 +0600

    initial commit

--- app/src/assets/js/script.js
+++ app/src/assets/js/script.js
@@ -4,19 +4,15 @@
 //= fragments/header/hamburger.js
 
 $(document).ready(function() {
-	// get current URL path and assign 'active' class
-    // var pathname = window.location.pathname;
-    // $('.nav__list > .nav__item > a').removeClass('nav__link--active');
-    // $('.nav__list > .nav__item > a[href="'+pathname+'"]').addClass('nav__link--active');
-    // console.log(pathname);
+	var loc = window.location.pathname;
+    if (loc !== '/') {
+        $('.nav__item').find('a').each(function() {
+            $(this).toggleClass('nav__link--active', $(this).attr('href') ==    loc);
+         });
+}
 
 })
 
-var loc = window.location.pathname;
-if (loc !== '/') {
-    $('.nav__item').find('a').each(function() {
-        $(this).toggleClass('nav__link--active', $(this).attr('href') == loc);
-     });
-}
+
 
   
