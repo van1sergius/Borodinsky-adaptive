c37c6843ceedbb709bee77abf2daf861c814b4cd
van <van1sergius@gmail.com>
2020-02-08 16:23:56 +0600

    signin animation done

--- app/src/assets/js/script.js
+++ app/src/assets/js/script.js
@@ -1,3 +1,7 @@
+// const enquire = require("enquire.js");
+
+// const enquire = require("enquire.js");//del
+
 //= ../../../../node_modules/jquery/dist/jquery.js
 //= ../../../../node_modules/enquire.js/dist/enquire.js
 //= fragments/features/slider.js
@@ -18,7 +22,69 @@ $(document).ready(function() {
     console.log(loc);
 }
 
+
+var popUp = 'zoomIn'; 
+
+$signin = $('.signin');
+
+enquire.register('screen and (max-width: 767px)', {
+    match : function() {
+        console.log('test');
+
+        $($signin).addClass('mobile');
+        if ($signin.hasClass(popUp)) {
+            $signin.removeClass(popUp);
+        }
+
+    },
+    unmatch : function() {
+        $($signin).removeClass('mobile');
+        if ($signin.hasClass(popUp)) {
+            $signin.removeClass(popUp);
+        }
+    }
+ 
+  });
+
+  enquire.register('screen and (min-width: 768px)', {
+    match : function() {
+        console.log('test1');
+        if ($signin.hasClass(popUp)) {
+            $signin.removeClass(popUp);
+        }
+    }
+
+  });
+
+$('.nav__link--login').on('click', function(event) {
+    event.preventDefault();
+
+    if ($signin.hasClass(popUp)) {
+        $signin.removeClass(popUp);
+    }
+
+    $($signin).toggleClass('signin--visible');
+
+
+    if (!$signin.hasClass('mobile')) {
+            $($signin).addClass(popUp);
+    };
+
+    
+
+    $('.nav__link').removeClass('nav__link--visible');
+    $('.hamburger').removeClass('is-active');
 })
+
+$('.signin__close').on('click', function(event) {
+    event.preventDefault();
+
+    $('.signin').removeClass('signin--visible');
+
+})
+
+})
+
 
 
 
